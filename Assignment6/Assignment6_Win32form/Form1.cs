91db25095e4450105d1c1107abffa7f43e662ff7
Vishnupriya <Vishnupriya.Chandralekhamohan@ust.com>
2024-10-24 21:47:38 +0530

    Assignment6

---
+++ Assignment6/Assignment6_Win32form/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Assignment6_Win32form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+    }  
+}
+
---
+++ Assignment6/InvokePythonScriptAssignment4/sum_script.py
@@ -0,0 +1,8 @@
+﻿# sum_script.py
+import sys
+def main(): 
+    a=int(sys.argv[1])   
+    b = int(sys.argv[2])
+    print(a + b)
+if __name__ == "__main__":
+    main()
